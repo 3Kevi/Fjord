2fb18a605f2ecbd2fb71850f38ff3a639f3312ba
Kevi <kkevi@proton.me>
2022-06-23 12:13:20 +0300

    Refactor the code

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Fjord
 Fjord is a Discord bot created as a practice in order to learn Lua.
-A lot of the things are hard coded, since there is no need to create a database for such a simple project to my mind.
 
 ## Running locally
 1. Clone this repository
--- src/fjord.lua
+++ src/fjord.lua
@@ -2,14 +2,15 @@ local discordia = require('discordia')
 local client = discordia.Client()
 
 client:on('ready', function()
-	print('Logged in as '.. client.user.username)
-    client:setGame('something')
+	client:setGame('a game')
+    print('Logged in as '.. client.user.username)
 end)
 
 local prefix = '.'
 
 client:on('messageCreate', function(message)
-	if message.content == prefix .. 'ping' then
+    -- Ping
+	if message.content == prefix..'ping' then
 		message:reply {
             content = 'Pong!',
             reference = {
@@ -17,7 +18,8 @@ client:on('messageCreate', function(message)
                 mention = false
             }
         }
-    elseif message.content == prefix .. 'pong' then
+    -- Pong
+    elseif message.content == prefix..'pong' then
         message:reply {
             content = 'Ping!',
             reference = {
@@ -25,11 +27,8 @@ client:on('messageCreate', function(message)
                 mention = false
             }
         }
-	end
-end)
-
-client:on('messageCreate', function (message)
-    if message.content == prefix .. 'colors' then
+    -- Name colours
+    elseif message.content == prefix..'colors' then
         message:reply {
             embed = {
                 title = 'Name colours',
@@ -47,7 +46,7 @@ client:on('messageCreate', function (message)
                 mention = false
             }
         }
-    end
+	end
 end)
 
 client:run('Bot '..os.getenv('BOT_TOKEN'))
