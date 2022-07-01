bcc4f4f9c1e6559730cfbd0f9b974ce1a184669b
Kevi <kkevi@proton.me>
2022-07-01 08:51:39 +0300

    Remove mentioning in replies

--- src/fjord.lua
+++ src/fjord.lua
@@ -13,19 +13,13 @@ client:on('messageCreate', function(message)
 	if message.content == prefix..'ping' then
 		message:reply {
             content = 'Pong!',
-            reference = {
-                message = message,
-                mention = false
-            }
+            reference = {message = message}
         }
     -- Pong
     elseif message.content == prefix..'pong' then
         message:reply {
             content = 'Ping!',
-            reference = {
-                message = message,
-                mention = false
-            }
+            reference = {message = message}
         }
     -- Name colours
     elseif message.content == prefix..'colors' then
@@ -34,17 +28,11 @@ client:on('messageCreate', function(message)
                 title = 'Name colours',
                 description = 'Here you can pick a fancy colour for your Discord name.',
                 fields = {
-                    {
-                        name = 'Available colours',
-                        value = os.getenv('ROLES')
-                    }
+                    {name = 'Available colours', value = os.getenv('ROLES')}
                 },
                 color = message.guild.me.highestRole.color
             },
-            reference = {
-                message = message,
-                mention = false
-            }
+            reference = {message = message}
         }
 	end
 end)
